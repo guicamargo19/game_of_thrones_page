e8b116d084881c291039abdd1fa81d4f98888fc3
camargo <gui.ferreira.camargo@gmail.com>
2023-12-21 15:24:36 -0300

    Configurado Gulp e ambiente de desenvolvimento e produção

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,32 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 
-function compiles() {
+function compileCSS() {
     return gulp.src("./src/styles/*.scss")
     .pipe(sass(
         { outputStyle: "compressed" }
     ))
-    .pipe(gulp.dest("./dist"))
+    .pipe(gulp.dest("./dist/css"))
+}
+
+function minifiedJS() {
+    return gulp.src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"))
+}
+
+function minifiedImages() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"))
 }
 
 
-exports.default = compiles;
+exports.default = gulp.parallel(compileCSS, minifiedJS, minifiedImages);
+
+exports.watch = function() {
+    gulp.watch("./src/styles/*.scss", gulp.parallel(compileCSS));
+    gulp.watch("./src/scripts/*.js", gulp.parallel(minifiedJS));
+}
